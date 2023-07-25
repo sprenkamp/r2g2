11d617982fa57d9e112cfb140f1e7c82351664c4
Xiaohw1999 <wuxiaohui990723@gmail.com>
2023-07-25 23:33:15 +0200

    1

--- frontend/r2g2_vue/src/components/Home.vue
+++ frontend/r2g2_vue/src/components/Home.vue
@@ -1,16 +1,108 @@
-<template>
+<!-- <template>
     <h3>This is home page</h3>
     <el-button color="FFFFFF" type="primary" class="button1">
       <router-link to="/newspage" class="news">news</router-link>
     </el-button>
+
+    <div>
+      <h1>{{ $t('Select a country of interest') }}</h1>
+      <p>{{ $t("all countries analysed") }}</p>
+      <button @click="changeLocale('English')">English</button>
+      <button @click="changeLocale('Deutsch')">Deutsch</button>
+    </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    changeLocale(locale) {
+      this.$i18n.locale = locale;
+    }
+  }
 };
+
 </script>
 
 <style>
+
+</style> -->
+
+<template>
+  <el-container class="layout-container" style="height: 700px">
+    <el-aside width="400px">
+      <el-scrollbar>
+        <div class="m-4">
+          <p>Sprache/Language</p>
+          <el-select v-model="value" class="m-2" placeholder="Select Language" size="large">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </div>
+        <div class="m-4">
+          <p>Choose the topics of interest within the telegram data</p>
+          <el-select
+            v-model="value1"
+            multiple
+            placeholder="Select"
+            size="large"
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </div>
+        <div class="m-4">
+          <p>Choose the topics of interest within the news data</p>
+          <el-select
+            v-model="value1"
+            multiple
+            placeholder="Select"
+            size="large"
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </div>
+
+      </el-scrollbar>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+      </el-header>
+      <el-main>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.layout-container .el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
+.layout-container .el-aside {
+  color: var(--el-text-color-primary);
+  background: var(--el-color-primary-light-8);
+}
+.layout-container .el-main {
+  padding: 0;
+}
 
 </style>
--- frontend/r2g2_vue/src/main.js
+++ frontend/r2g2_vue/src/main.js
@@ -1,14 +1,26 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
+import translate_app from './data/translate_app.json'
 import App from './App.vue';
 import router from './router/router';
 import axios from "axios";
 import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
 
+// language
+const i18n = createI18n({
+    legacy: true,
+    locale: 'English',
+    messages: translate_app,
+    missingWarn: false
+});
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.use(ElementPlus);
 app.use(router);
+app.use(i18n)
 app.component("Loading", Loading);
 app.mount('#app');
