2c833ff976de1bb83d58eb5d3051294a35368370
Xiaohw1999 <wuxiaohui990723@gmail.com>
2023-08-03 00:38:32 +0200

    line chart

---
+++ frontend/r2g2_vue/src/components/LineChart.vue
@@ -0,0 +1,44 @@
+<template>
+    <Line :data="chartData" :options="chartOptions" />
+  </template>
+  
+  <script>
+  import { Line } from 'vue-chartjs';
+  import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement, TimeScale, Colors} from 'chart.js';
+  import 'chartjs-adapter-date-fns';
+
+  ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement, TimeScale, Colors)
+  
+  export default {
+    name: 'LineChart',
+    components: { Line },
+    props: {
+      chartData: {
+        type: Object,
+        required: true
+      },
+      chartOptions: {
+        type: Object,
+        default: () => ({
+          responsive: true,
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day',
+                displayFormats: {
+                  day: 'yyyy-MM-dd',
+                },
+              },
+            },
+            y: {
+              beginAtZero: true,
+            },
+          },
+        })
+      }
+    },
+  }
+  
+  </script>
+  
