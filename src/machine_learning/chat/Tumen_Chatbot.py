76fb3097e6d37b1e97e16c458de00bd1ddd4109d
Tumen Dambiev <73483798+Tumeshka@users.noreply.github.com>
2023-09-15 09:55:02 +0200

    Changed the code to have API in it and MONGODBATLASVECTORSEARCH. HAs issues with API calling in a continous fashion

--- src/machine_learning/chat/Tumen_Chatbot.py
+++ src/machine_learning/chat/Tumen_Chatbot.py
@@ -1,63 +1,82 @@
-import csv
 import os
-import openai
+
+from langchain.prompts import PromptTemplate
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import FAISS
-from langchain.vectorstores.base import Document
+from langchain.memory import ConversationBufferMemory 
+from langchain.vectorstores import MongoDBAtlasVectorSearch
 
 from dotenv import load_dotenv
+from pymongoarrow.monkey import patch_all
+from pymongo import MongoClient
+from fastapi import BackgroundTasks,FastAPI
+
+app = FastAPI()
+
 load_dotenv()
-import datetime
 
 ATLAS_TOKEN = os.environ["ATLAS_TOKEN"]
 ATLAS_USER = os.environ["ATLAS_USER"]
 
 #MongoDB part start
-import numpy
-import pymongo
-import pymongoarrow
-from pymongo import MongoClient
 
 client = MongoClient(
     "mongodb+srv://{}:{}@cluster0.fcobsyq.mongodb.net/".format(
         ATLAS_USER, ATLAS_TOKEN))
 
 db = client["test"]
-col = db["telegram"]
+col = db["telegram_sample"]
 
-  
-from pymongoarrow.monkey import patch_all
 patch_all()
 
-import pandas as pd
 df = col.find_pandas_all({})
 df.to_csv('csvfile.csv')
 
 #MongoDB part end
 
-def read_csv_into_vector_document(file, text_cols):
-    with open(file, newline='') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        text_data = []
-        for row in csv_reader:
-            text = ' '.join([row[col] for col in text_cols])
-            text_data.append(text)
-        return [Document(page_content=text) for text in text_data]
-
 api_key = os.environ.get('OPENAI_API_KEY')
-
 if not api_key:
     print('OpenAI API key not found in environment variables.')
     exit()
 
-data = read_csv_into_vector_document('csvfile.csv', ["_id","chat","channel_id","messageDatetime","update_time","messageText",])
 embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-vectors = FAISS.from_documents(data, embeddings)
-chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=api_key), retriever=vectors.as_retriever())
-history = []
+vectors = MongoDBAtlasVectorSearch(
+    collection=col, text_key='messageText',
+    embedding=embeddings, index_name='telegram_embedding'
+)
 
-while True:
-    query = input("Enter Your Query:")
-    print(chain({"question": query, "chat_history": history})["answer"])
+memory = ConversationBufferMemory( 
+memory_key='chat_history', 
+return_messages=True, 
+output_key='answer')
+
+llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=api_key)
+
+
+prompt_template = """Use the following pieces of context to answer the question at the end. 
+Combine the information from the context with your own general knowledge to provide a comprehensive and accurate answer. 
+Please be as specific as possible, also you are a friendly chatbot who is always polite.
+{context}
+Question: {question}"""
+
+QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt_template)
+
+chain = ConversationalRetrievalChain.from_llm(
+    llm=llm, 
+    retriever=vectors.as_retriever(search_type = 'mmr', search_kwargs={'k': 5, 'lambda_mult': 0.25}), 
+    memory = memory,
+    return_source_documents=True,
+    return_generated_question=True,
+    combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+chat_history = []
+
+@app.post("/query")
+def query(chat_history):
+    while True:
+        query = input("Enter Your Query:")
+        metadata = vectors.max_marginal_relevance_search(query, k = 3, fetch_k= 5)
+        answer = chain({"question": query, "chat_history": chat_history})["answer"]
+        chat_history = [(query, answer)]
+        return answer
