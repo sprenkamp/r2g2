e2336d5c7106effc257177f19e5f31614e0dbc8e
zhengyuan pan <zhengyuanpan@outlook.com>
2023-09-18 14:33:27 +0200

    add tqdm, update the seed_topic_list

--- src/machine_learning/BERTopic/runsemi-supervised-bert.py
+++ src/machine_learning/BERTopic/runsemi-supervised-bert.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 
 nltk.download('stopwords')
 
@@ -21,27 +22,23 @@ for stopwords in ukrstopWords:
     stopWords.append(stopwords)
 
 seed_topic_list = {
-    "Volunteering": ["volunteer", "donate", "help"],
-    "Integration": ["community", "integrate", "society"],
-    "Accommodation": ["accommodation", "housing", "shelter"],
-    "Immigration Procedure": ["immigration", "migration", "procedure"],
-    "Social Services": ["service", "benefit", "welfare"],
-    "Transport to and from Ukraine": ["transport", "bus", "train"],
-    "Geopolitical Policy and Sanctions": ["geopolitics", "sanction", "Russia"],
-    "Religious Guidance": ["ecclesiastical", "canonical", "church"],
-    "Consultate Service": ["consulate", "embassy", "diplomatic"],
-    "Banking": ["bank", "finance", "account"],
-    "Public Transportation and Services": ["public transport", "bus", "metro"],
-    "Education": ["school", "education", "university"],
-    "Job and Development Opportunities": ["job", "employment", "opportunity"],
-    "Interpretation and Translation Services": ["interpretation", "translation", "language"],
-    "Carriers": ["carrier", "transport", "logistics"],
-    "Medical Information": ["medical", "health", "hospital"],
-    "Asylum": ["asylum", "protection", "application"],
-    "Transport Personal Car": ["car", "vehicle", "drive"],
-    "Dentistry Information": ["dentistry", "dentist", "tooth"],
-    "Network Provider": ["network", "provider", "internet"],
-    "Veterinarian and Vaccination": ["veterinarian", "vet", "animal"]
+    "Volunteering": ["volunteer", "donate", "help", "support", "aid", "assist", "contribute", "доброволец", "пожертвовать", "помощь", "поддержка", "вклад", "Freiwilliger", "spenden", "Hilfe", "Unterstützung", "Beitrag", "assistieren", "beitragen"],
+    "Integration": ["community", "integrate", "society", "culture", "local", "mingle", "adapt", "сообщество", "интеграция", "культура", "локальный", "общаться", "адаптировать", "Gemeinschaft", "integrieren", "Gesellschaft", "Kultur", "lokal", "mischen", "anpassen"],
+    "Accommodation": ["accommodation", "housing", "shelter", "stay", "rent", "lease", "residence", "accomodation", "жилье", "проживание", "убежище", "аренда", "квартира", "Unterkunft", "Wohnung", "Schutz", "Aufenthalt", "Miete", "Pacht", "Wohnsitz"],
+    "Immigration Procedure": ["immigration", "migration", "procedure", "passport", "visa", "status", "citizenship", "иммиграция", "миграция", "процедура", "паспорт", "виза", "гражданство", "Einwanderung", "Migration", "Verfahren", "Pass", "Visum", "Status", "Staatsangehörigkeit"],
+    "Social Services": ["service", "benefit", "welfare", "support", "aid", "assistance", "children", "услуга", "поддержка", "благосостояние", "помощь", "дети", "Dienst", "Vorteil", "Sozialhilfe", "Unterstützung", "Hilfe", "Assistenz", "Kinder"],
+    "Transport to and from Ukraine": ["transport", "bus", "train", "flight", "route", "border", "Ukraine", "crossing", "транспорт", "автобус", "поезд", "рейс", "маршрут", "граница", "Украина", "Transport", "Bus", "Zug", "Flug", "Strecke", "Grenze", "Ukraine", "Überquerung"],
+    "Geopolitical Policy and Sanctions": ["geopolitics", "sanction", "Russia", "tactic", "strategy", "military", "diplomatic", "countermeasure", "economic penalty", "trade restriction", "retaliation", "policy", "war", "геополитика", "санкция", "Россия", "тактика", "стратегия", "военный", "дипломатический", "контрмера", "экономическое наказание", "торговое ограничение", "ответные меры", "война", "Geopolitik", "Sanktion", "Russland", "Taktik", "Strategie", "Militär", "diplomatisch", "Gegenmaßnahme", "wirtschaftliche Strafe", "Handelsbeschränkung", "Vergeltung", "Krieg"],
+    "Religious Guidance": ["ecclesiastical", "canonical", "church", "doctrine", "clergy", "sermon", "edict", "declaration", "faith guideline", "pastoral message", "Orthodox", "церковный", "канонический", "церковь", "доктрина", "духовенство", "проповедь", "указ", "декларация", "руководство веры", "пастырское послание", "Православный", "kirchlich", "kanonisch", "Kirche", "Doktrin", "Klerus", "Predigt", "Edikt", "Erklärung", "Glaubensrichtlinie", "pastorale Botschaft", "Orthodox"],
+    "Consultate Service": ["consulate", "embassy", "diplomatic", "service", "official", "document", "representative", "консульство", "посольство", "дипломатический", "служба", "официальный", "документ", "Konsulat", "Botschaft", "diplomatisch", "Dienst", "offiziell", "Dokument", "Vertreter"],
+    "Banking": ["bank", "finance", "account", "transfer", "currency", "loan", "credit", "банк", "финансы", "счет", "перевод", "валюта", "кредит", "Bank", "Finanzen", "Konto", "Überweisung", "Währung", "Kredit"],
+    "Public Transportation and Services": ["public transport", "bus", "metro", "taxi", "ticket", "route", "schedule", "общественный транспорт", "автобус", "метро", "такси", "билет", "маршрут", "расписание", "öffentlicher Verkehr", "Bus", "U-Bahn", "Taxi", "Ticket", "Route", "Fahrplan"],
+    "Education": ["school", "education", "university", "study", "student", "teacher", "course", "book", "learn", "школа", "образование", "университет", "учеба", "студент", "учитель", "курс", "книга", "учиться", "Schule", "Bildung", "Universität", "Studium", "Student", "Lehrer", "Kurs", "Buch", "lernen"],
+    "Medical": ["hospital", "doctor", "clinic", "medicine", "health", "treatment", "surgery", "больница", "врач", "клиника", "лекарство", "здоровье", "лечение", "хирургия", "Krankenhaus", "Arzt", "Klinik", "Medizin", "Gesundheit", "Behandlung", "Chirurgie"],
+    "Legal": ["law", "legal", "court", "judge", "lawyer", "rights", "regulation", "закон", "право", "суд", "судья", "адвокат", "права", "регулирование", "Gesetz", "Recht", "Gericht", "Richter", "Anwalt", "Rechte", "Regelung"],
+    "Job and Employment": ["job", "work", "employment", "hire", "salary", "office", "profession", "робота", "работа", "занятость", "наем", "зарплата", "офис", "профессия", "Job", "Arbeit", "Beschäftigung", "einstellen", "Gehalt", "Büro", "Beruf"],
+    "Culture and Leisure": ["culture", "art", "museum", "theater", "music", "book", "festival", "культура", "искусство", "музей", "театр", "музыка", "книга", "фестиваль", "Kultur", "Kunst", "Museum", "Theater", "Musik", "Buch", "Festival"],
+    "Shopping and Commerce": ["shop", "store", "mall", "buy", "sell", "product", "retail", "магазин", "торговый центр", "покупка", "продажа", "товар", "розница", "Shop", "Geschäft", "Einkaufszentrum", "kaufen", "verkaufen", "Produkt", "Einzelhandel"]
 }
 
 class BERTopicAnalysis:
@@ -57,7 +54,7 @@ class BERTopicAnalysis:
 
     def get_labels_from_seed(self):
         labels = []
-        for text in self.df['text']:
+        for text in tqdm(self.df['text'], desc="Assigning labels"):
             assigned = False
             for topic, keywords in seed_topic_list.items():
                 if any(keyword in text for keyword in keywords):
@@ -69,8 +66,15 @@ class BERTopicAnalysis:
         return labels
 
     def get_document_embeddings(self):
-        model = SentenceTransformer('paraphrase-MiniLM-L6-v2')  
-        embeddings = model.encode(self.df['text'].tolist())
+        model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+        texts = self.df['text'].tolist()
+
+        with tqdm(total=len(texts), desc="Generating embeddings") as pbar:
+            def update_pbar(*args, **kwargs):
+                pbar.update(len(texts) // 10) 
+
+            embeddings = model.encode(texts, show_progress_bar=update_pbar)
+    
         return embeddings
 
     def fit_BERTopic(self):
