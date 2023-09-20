bc8e007110c37cc3ac179e320dbc920189c87acb
zhengyuan pan <91552192+loveRocco@users.noreply.github.com>
2023-09-20 11:39:04 +0200

    Update runsemi-supervised-bert.py

--- src/machine_learning/BERTopic/runsemi-supervised-bert.py
+++ src/machine_learning/BERTopic/runsemi-supervised-bert.py
@@ -2,22 +2,23 @@ from bertopic import BERTopic
 import pandas as pd
 
 # Load your dataset
-df_telegram_concat = pd.read_csv(r"C:\Users\rocco\Documents\project0809\r2g2\src\machine_learning\BERTopic\df_telegram_concat.csv", encoding='ISO-8859-1')
+df_telegram_concat = pd.read_csv(r"C:\Users\rocco\Documents\project0809\r2g2\src\machine_learning\BERTopic\df_telegram_concat.csv", encoding='UTF-8')
 
-# Sample data
-sample_df = df_telegram_concat.sample(n=500, random_state=42)
-sample_docs = sample_df['text'].tolist()
-sample_y = sample_df['cluster_id'].tolist()
+# (Non)Sample data
+#sample_df = df_telegram_concat.sample(n=20000, random_state=42)
+df_data = df_telegram_concat  # Changed the name for clarity since it's not a sample
+docs = df_data['text'].tolist()
+labels = df_data['cluster_id'].tolist()
 
 # Create BERTopic model with a multilingual sentence transformer model
-topic_model_sample = BERTopic(embedding_model="distiluse-base-multilingual-cased", verbose=True)
+topic_model = BERTopic(embedding_model="paraphrase-multilingual-MiniLM-L12-v2", verbose=True, nr_topics="auto", min_topic_size=100)
 
 # Train the model
-topic_model_sample.fit(sample_docs, y=sample_y)
-sample_topic_words = topic_model_sample.get_topic_info()
-print(sample_topic_words)
+topic_model.fit(docs, y=labels)
+topic_words = topic_model.get_topic_info()
+print(topic_words)
 
-save_path = "C:\\Users\\rocco\\Documents\\project0809\\r2g2\\src\\machine_learning\\bertopic_model"
+save_path = r"C:\Users\rocco\Documents\project0809\r2g2\src\machine_learning\bertopic_model.safetensors"  # Added the .safetensors extension
 
-# Save the entire BERTopic model
-topic_model_sample.save(save_path, save_embedding_model=True)
+# Save the BERTopic model in .safetensors format
+topic_model.save(save_path, save_embedding_model=True)  
