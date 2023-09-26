eecb43cda1002067bfe343c949b0c19b4699ef32
zhengyuan pan <91552192+loveRocco@users.noreply.github.com>
2023-09-26 20:26:57 +0200

    Update runsemi-supervised-bert.py
    1. add cuml
    2. add vectorizer_model
    3. structure the class

--- src/machine_learning/BERTopic/runsemi-supervised-bert.py
+++ src/machine_learning/BERTopic/runsemi-supervised-bert.py
@@ -1,24 +1,56 @@
-from bertopic import BERTopic
+import os
 import pandas as pd
+from bertopic import BERTopic
+from sklearn.feature_extraction.text import CountVectorizer
+from stoppingword_process import stopWords
+from cuml.cluster import HDBSCAN
+from cuml.manifold import UMAP
 
-# Load your dataset
-df_telegram_concat = pd.read_csv(r"C:\Users\rocco\Documents\project0809\r2g2\src\machine_learning\BERTopic\df_telegram_concat.csv", encoding='UTF-8')
+class OurBERTopicModel:
+    def __init__(self, output_folder):
+        self.output_folder = output_folder
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+    
+    def fit_model(self, docs, labels):
+        umap_model = UMAP(n_neighbors=25, n_components=10, metric='cosine', low_memory=False, random_state=42)
+        hdbscan_model = HDBSCAN(min_cluster_size=100, metric='euclidean', prediction_data=True)
+        vectorizer_model = CountVectorizer(ngram_range=(1, 2), stop_words=stopWords)
+        self.model = BERTopic(embedding_model="paraphrase-multilingual-MiniLM-L12-v2", 
+                              language="multilingual",
+                              verbose=True, 
+                              nr_topics="auto",
+                              umap_model=umap_model, 
+                              hdbscan_model=hdbscan_model,
+                              vectorizer_model=vectorizer_model) 
+        self.model.fit(docs, y=labels)
+        self.save_results()
+        self.write_representative_docs_df()
+        
+    def save_results(self):
+        fig = self.model.visualize_topics()
+        fig.write_html(f"{self.output_folder}/bert_topic_model_distance_model.html")
+        fig = self.model.visualize_hierarchy()
+        fig.write_html(f"{self.output_folder}/bert_topic_model_hierarchical_clustering.html")
+        fig = self.model.visualize_barchart(top_n_topics=30)
+        fig.write_html(f"{self.output_folder}/bert_topic_model_word_scores.html")
+        fig = self.model.visualize_heatmap()
+        fig.write_html(f"{self.output_folder}/bert_topic_model_word_heatmap.html")
+        self.model.save(f"{self.output_folder}/BERTopicmodel")
+        
+    def write_representative_docs_df(self):
+        writer = pd.ExcelWriter(f"{self.output_folder}/representative_docs.xlsx", engine='xlsxwriter')
+        for i in self.model.get_representative_docs().keys():
+            df = pd.DataFrame(self.model.get_representative_docs()[i], columns=['message'])
+            df.to_excel(writer, sheet_name=self.model.get_topic_info()[self.model.get_topic_info()['Topic'] == i]['Name'].values[0][:31])
+        writer.save()
+        self.model.get_topic_info().to_csv(f"{self.output_folder}/topic_info.csv")
 
-# (Non)Sample data
-#sample_df = df_telegram_concat.sample(n=20000, random_state=42)
-df_data = df_telegram_concat  # Changed the name for clarity since it's not a sample
-docs = df_data['text'].tolist()
-labels = df_data['cluster_id'].tolist()
-
-# Create BERTopic model with a multilingual sentence transformer model
-topic_model = BERTopic(embedding_model="paraphrase-multilingual-MiniLM-L12-v2", verbose=True, nr_topics="auto", min_topic_size=100)
-
-# Train the model
-topic_model.fit(docs, y=labels)
-topic_words = topic_model.get_topic_info()
-print(topic_words)
-
-save_path = r"C:\Users\rocco\Documents\project0809\r2g2\src\machine_learning\bertopic_model.safetensors"  # Added the .safetensors extension
-
-# Save the BERTopic model in .safetensors format
-topic_model.save(save_path, save_embedding_model=True)  
+if __name__ == "__main__":
+    df_telegram_concat = pd.read_csv("src/machine_learning/BERTopic/df_telegram_concat.csv", encoding='UTF-8')
+    docs = df_telegram_concat['text'].tolist()
+    labels = df_telegram_concat['cluster_id'].tolist()
+    
+    output_folder = "src/machine_learning/BERTopic/Result"
+    bertopic_model = OurBERTopicModel(output_folder)
+    bertopic_model.fit_model(docs, labels)
