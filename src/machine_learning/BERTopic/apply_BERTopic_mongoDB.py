51e33c37fcfc0db30696ff6f2a6422c72ecd3733
Kilian Sprenkamp <kiliansprenkamp@vpn-10-21-18-129.uzh.ch>
2023-10-04 15:43:37 +0200

    adding prediction BERTopic to mongoDB script

--- src/machine_learning/BERTopic/apply_BERTopic_mongoDB.py
+++ src/machine_learning/BERTopic/apply_BERTopic_mongoDB.py
@@ -1,56 +1,53 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, UpdateOne
 import pandas as pd
 from bertopic import BERTopic
 import os
 from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
-def predict_and_update(model_path):
-    # 1. Connect to MongoDB and fetch the data
-    
-    # Load .env variables
+def fetch_data():
+    global col
     load_dotenv('.env')
-
-    # Access variables from .env file
     ATLAS_USER = os.getenv("ATLAS_USER")
     ATLAS_TOKEN = os.getenv("ATLAS_TOKEN")
-
-    client = MongoClient(
-        "mongodb+srv://{}:{}@cluster0.fcobsyq.mongodb.net/".format(
-            ATLAS_USER, ATLAS_TOKEN))
-
+    client = MongoClient("mongodb+srv://{}:{}@cluster0.fcobsyq.mongodb.net/".format(ATLAS_USER, ATLAS_TOKEN))
     db = client["scrape"]
     col = db["telegram"]
-
-    # Query
-    query = {
-        "$and": [
-            {"country": "Switzerland"}
-        ]
-    }
-
-    # Execute Query
+    query = {"$and": [{"country": "Switzerland"}]}
     cursor = col.find(query)
-
-    # 2. Convert MongoDB data to a pandas dataframe
     df = pd.DataFrame(list(cursor))
-    
-    # Assume the column containing the text data is named 'text'
+    return df, client  # Return the client as well
+
+def predict_topics(df, model_path):
     texts = df['messageText'].tolist()
-
-    # 3. Load your pre-trained BERTopic model
     topic_model = BERTopic.load(model_path)
-
-    # 4. Predict the class (topic) for each instance
     topics, _ = topic_model.transform(texts)
     df['predicted_class'] = topics
+    return df
 
-    # 5. Update MongoDB records with the predicted class
-    for index, row in df.iterrows():
-        col.update_one({"_id": row["_id"]}, {"$set": {"predicted_class": row["predicted_class"]}})
+def update_chunk(chunk):
+    bulk_operations = [UpdateOne({"_id": row["_id"]}, {"$set": {"predicted_class": row["predicted_class"]}}) for index, row in chunk.iterrows()]
+    col.bulk_write(bulk_operations)
+
+def predict_and_update(model_path):
+    df, client = fetch_data()  # Get the client as well
+    print("data loaded")
+    print(df.shape)
+    # df = df[:3000]
+    df = predict_topics(df, model_path)
+    print("topics predicted")
+
+    # Splitting dataframe into chunks of 1000
+    chunks = [df.iloc[i:i+1000] for i in range(0, len(df), 1000)]
+
+    # Using ThreadPoolExecutor to update chunks in parallel
+    with ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(update_chunk, chunks), total=len(chunks)))
 
     print("Records updated successfully!")
     
-    client.close()
+    client.close()  # Close the client here after all operations are done
 
 # Example usage
 model_path = "kdot/BERTopicTelegramAnalysis"
