f93058fc73a7952e7ec3db55076177173d770a81
Xiaohw1999 <wuxiaohui990723@gmail.com>
2023-11-07 13:34:23 +0100

    latest，add new url

--- frontend/mongoDB-backend/connect.js
+++ frontend/mongoDB-backend/connect.js
@@ -1,14 +1,35 @@
+// scp -i "D:\aws_key\aws_node.pem" "D:\visualstudiocode\project\r2g2\r2g2\frontend\mongoDB-backend\connect.js" ec2-user@ec2-51-20-75-190.eu-north-1.compute.amazonaws.com:/home/ec2-user/
+
 const express = require('express');
 const compression = require('compression');
 const { MongoClient } = require('mongodb');
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors")
+const https = require("https")
 dotenv.config()
 app.use(compression());
 
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://main--coruscating-alpaca-1de6ac.netlify.app',
+  'https://governmentasaplatform.ch',
+  'https://www.governmentasaplatform.ch',
+  'https://db.governmentasaplatform.ch',
+  'www.governmentasaplatform.ch',
+  'governmentasaplatform.ch',
+  'ELB-r2g2-802525093.eu-north-1.elb.amazonaws.com',
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
@@ -20,19 +41,20 @@ app.get('/:databaseName/:collectionName', async (req, res) => {
     const uri = process.env.MONGO_URL;
     const client = new MongoClient(uri);
     await client.connect();
-
     const database = client.db(databaseName);
     const collection = database.collection(collectionName);
-
     const collectionData = await collection.find().toArray();
-
-    await client.close();
-
     res.json(collectionData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching data.' });
+  } finally {
+    client.close();
   }
+});
+
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
 });
 
 // run serve
