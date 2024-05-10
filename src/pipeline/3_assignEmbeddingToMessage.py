50ee58d134cf0954901882427126346afe7ccdd9
yuyuLJY <jiayu1922@163.com>
2024-05-10 17:29:09 +0200

    increase request interval to 0.3s

--- src/pipeline/3_assignEmbeddingToMessage.py
+++ src/pipeline/3_assignEmbeddingToMessage.py
@@ -14,7 +14,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def get_embedding(text, model="text-embedding-ada-002"):
     text = text.replace("\n", " ")
-    time.sleep(0.2)
+    time.sleep(0.3)
     embedding = openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
     return embedding
 
